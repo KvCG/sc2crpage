96a7fa1efe2748c98a79a364907d3fa3c8a32712
Kevin Chaves <Kevcg16@gmail.com>
2024-08-24 19:45:31 -0600

    Test deploy

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
             <h1>SC2 CR</h1>
             <div className="card">
                 <button onClick={() => setCount(count => count + 1)}>
-                    Kerverus is awesome x{count}
+                    Kary is awesome x{count}
                 </button>
             </div>
         </>
