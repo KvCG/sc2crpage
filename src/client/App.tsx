7664f14adda6e3236c410472474ee5fee174fa89
Kevin Chaves <Kevcg16@gmail.com>
2024-08-27 00:05:41 -0600

    Adds material UI

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,26 +1,59 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { search } from './services/getPlayerInfo.ts'
 import './App.css'
 import { AxiosResponse } from 'axios'
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid'
+import pi from '../../mockData/PlayerInfo.json'
 
 // Define functional component using TypeScript
 const App: React.FC = () => {
     // TypeScript infers the type of `count` as number
     const [inputValue, setInputValue] = useState<string>('')
     const [playerInfo, setPlayerInfo] = useState<object[]>([])
+    const [rows, setRows] = useState<GridRowsProp>([])
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement
-		setInputValue(target.value)
+        setInputValue(target.value)
     }
+
+    useEffect(() => {
+        playerInfo.forEach((player, index) => {
+            addRow({
+                id: index,
+                col1: player.members.account.battleTag,
+                col2: String(player.ratingMax),
+            })
+        })
+    }, [playerInfo])
+
+    const addRow = (newRow: { id: number; col1: string; col2: string }) => {
+        setRows(prevRows => [...prevRows, newRow])
+    }
+
+
+    // pi.forEach((player, index)=>{
+    // 	addRow({ id: index, col1: player.members.account.battleTag, col2: String(player.ratingMax)})
+    // })
+
+    // pi.map((player, i) => {
+    //     addRow({ id: i, col1: player.members.account.battleTag, col2: String(player.ratingMax)})
+    // })
+    // const rows: GridRowsProp = [
+    //     { id: 1, col1: pi[0].members.account.battleTag , col2: 'World' },
+    //     { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    //     { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    // ]
+
+    const columns: GridColDef[] = [
+        { field: 'col1', headerName: 'Battletag', width: 150 },
+        { field: 'col2', headerName: 'Highest MMR', width: 150 },
+    ]
 
     return (
         <>
             <h1>SC2 CR </h1>
             <div className="card">
-                <input
-                    type="text"
-                    onInput={handleInput}
-                />
+                <input type="text" onInput={handleInput} />
                 <button
                     onClick={async () => {
                         const response: AxiosResponse = await search(inputValue)
@@ -32,7 +65,10 @@ const App: React.FC = () => {
             </div>
             <div>
                 <h2>Player Data</h2>
-                {JSON.stringify(playerInfo)}
+                {/* {JSON.stringify(playerInfo)} */}
+                <div style={{ height: 300, width: '100%' }}>
+                    <DataGrid rows={rows} columns={columns} />
+                </div>
             </div>
         </>
     )
