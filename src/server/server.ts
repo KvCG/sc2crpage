750dc3ad6587bc2b39d3423b960f2813426d4a52
Kevin Chaves <Kevcg16@gmail.com>
2024-09-05 18:15:41 -0600

    Adds port binding for render

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,7 +8,7 @@ import chokidar from 'chokidar'
 import 'dotenv/config'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const wsPort = 4000 // Port for WebSocket server
 
 // WebSocket server for development reloads
