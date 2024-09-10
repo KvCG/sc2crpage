678d3852902d5b812dcb2c95f70b257ff0b7f7b0
Kevin Chaves <Kevcg16@gmail.com>
2024-09-10 10:28:26 -0600

    Small fix

--- src/server/utils/cache.ts
+++ src/server/utils/cache.ts
@@ -1,14 +1,14 @@
 export const getTimeUntilNextRefresh = () => {
     const now = new Date()
-    let nextRefresh = new Date(now)
-    
-    // If the current time is after 24:00 (i.e., midnight), move the refresh to the next day.
-    if (now.getHours() === 24 && now.getMinutes() === 0) {
+    const nextRefresh = new Date() // Create a new Date object representing the current time
+
+    // Set nextRefresh to midnight (00:00) of the current day
+    nextRefresh.setHours(0, 0, 0, 0)
+
+    // If the current time is past midnight, move nextRefresh to the next day
+    if (now.getTime() >= nextRefresh.getTime()) {
         nextRefresh.setDate(now.getDate() + 1)
     }
-    
-    // Set the next refresh time to midnight (00:00) of the next day
-    nextRefresh.setHours(24, 0, 0, 0)
 
     const timeUntilRefresh = nextRefresh.getTime() - now.getTime() // in ms
     return timeUntilRefresh
