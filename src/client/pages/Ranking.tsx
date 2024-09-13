01a06a8e8eccbf552825aae76547331489979fbf
Kevin Chaves <Kevcg16@gmail.com>
2024-09-12 22:36:47 -0600

    Adds text for failling to retrieve top information

--- src/client/pages/Ranking.tsx
+++ src/client/pages/Ranking.tsx
@@ -42,6 +42,9 @@ export const Ranking = () => {
         if (error) {
             return <p>{error}</p>
         }
+		if (!currentData?.length) {
+			return <p>Slow network, please refresh the page</p>
+		}
         if (currentData || loading) {
             return <RankingTable data={currentData} loading={loading} />
         }
