b308680d88ef8bd8800f3086617a6781049c432b
Kevin Chaves <Kevcg16@gmail.com>
2024-09-12 21:41:11 -0600

    Adjusts snapshot expiration time

--- src/server/utils/cache.ts
+++ src/server/utils/cache.ts
@@ -2,9 +2,7 @@ export const getTimeUntilNextRefresh = () => {
     const now = new Date()
     const nextRefresh = new Date()
 
-    // Adjust for the Costa Rica time zone difference (UTC-6)
-    const timeZoneOffset = -6 // UTC-6 for Costa Rica
-    nextRefresh.setUTCHours(24 + timeZoneOffset, 0, 0, 0) // Set to 12 AM Costa Rica time
+    nextRefresh.setUTCHours(6, 0, 0, 0); // 6 AM UTC = 12 AM UTC-6 (Costa Rica time)
 
     // If the current time is past 12 AM local time, set the next refresh for the next day
     if (now.getTime() >= nextRefresh.getTime()) {
