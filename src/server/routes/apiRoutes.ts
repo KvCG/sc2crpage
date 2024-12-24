14da42a2ecd043bfbf8a9c89d3061eaaf52f9537
Carlos Cartin Cascante <alexabr13@hotmail.com>
2024-12-23 20:44:40 -0600

    All the server changes for the new replays view

--- src/server/routes/apiRoutes.ts
+++ src/server/routes/apiRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import pulseRoutes from './pulseRoutes'
 import challongeRoutes from './challongeRoutes'
 import utilityRoutes from './utilityRoutes'
+import googleRoutes from './googleRoutes'
 
 const router = Router()
 
 router.use('/', pulseRoutes)
 router.use('/', challongeRoutes)
 router.use('/', utilityRoutes)
+router.use('/', googleRoutes)
 
 export default router
