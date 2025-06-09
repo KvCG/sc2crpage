2bb1122f9f876b0509419dde455535f4efd97755
Carlos Cartin Cascante <carlos.cartincascante@gmail.com>
2025-06-08 20:22:54 -0600

    Apply the correct installation for the LRU library

--- src/server/utils/cache.ts
+++ src/server/utils/cache.ts
@@ -1,11 +1,11 @@
-import LRU from 'lru-cache'
+import { LRUCache } from 'lru-cache'
 
 /**
  * Creates an LRU cache instance for in-memory caching.
  * Only one value is ever stored (max: 1).
  * The cache will automatically delete expired entries after 30 seconds (ttl: 30_000 ms).
  */
-const cache = new LRU<string, any>({
+const cache = new LRUCache<string, any>({
     max: 1,
     ttl: 30_000 // 30 seconds
 })
