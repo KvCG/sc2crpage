3951a07ce1143aacb08a2fc5bb1a4b5ea282c429
Kevin <devcg16@gmail.com>
2025-09-20 21:26:59 -0600

    chore(eslint): convert flat config to ESM export for type:module

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,36 @@
-const js = require('@eslint/js')
-const globals = require('globals')
-const reactHooks = require('eslint-plugin-react-hooks')
-const reactRefresh = require('eslint-plugin-react-refresh')
-const tseslint = require('typescript-eslint')
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-module.exports = {
-    overrides: [
-        {
-            files: ['**/*.{js,mjs,cjs}'],
-            languageOptions: {
-                ecmaVersion: 2020,
-                globals: globals.browser,
-                sourceType: 'module',
-            },
-            extends: [js.configs.recommended],
+export default [
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            sourceType: 'module',
         },
-        {
-            files: ['**/*.{ts,tsx}'],
-            languageOptions: {
-                ecmaVersion: 2020,
-                globals: globals.browser,
-            },
-            extends: [js.configs.recommended, ...tseslint.configs.recommended],
-            plugins: {
-                'react-hooks': reactHooks,
-                'react-refresh': reactRefresh,
-            },
-            rules: {
-                ...reactHooks.configs.recommended.rules,
-                'react-refresh/only-export-components': [
-                    'warn',
-                    { allowConstantExport: true },
-                ],
-            },
+        extends: [js.configs.recommended],
+    },
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
-    ],
-}
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+        },
+    },
+]
